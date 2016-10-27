bcb1bd9b35c2542074265c2655976df7970ac6fd
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 16:46:34 +0300

    Homework12_v2

--- Home12.py
+++ Home12.py
@@ -2,7 +2,16 @@
 number_input = int(input("Введите число для проверки на четность: "))
 
 
-# Функция
+# Функция побитового сравнения
+if int(bin(number_input)[-1]) == 0:
+    print("Число %d четное, bit" % number_input)
+elif int(bin(number_input)[-1]) == 1:
+    print("Число %d нечетное, bit" % number_input)
+else:
+    print("Error input")
+
+
+# Функция сравнения по остатку модуля
 if number_input % 2 == 0:
     print("Число %d четное" % number_input)
 elif number_input % 2 != 0:
