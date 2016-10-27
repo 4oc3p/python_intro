090985e9bfc9fcd96ff9a58c0e77dd4fde26f590
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 16:10:59 +0300

    Homework9_v2

--- Home9.py
+++ Home9.py
@@ -3,9 +3,13 @@ import math
 
 # Функция перевода градусов в радианы
 def grad2rad(grad):
-    return math.cos(grad * math.pi / 180)
+    return grad * math.pi / 180
+
+
+# Функция cos(rad)
+def cos_rad(rad):
+    return math.cos(rad)
 
 
 # Вывод результата
-print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (
-    grad2rad(60), grad2rad(45), grad2rad(40)))
+print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (cos_rad(grad2rad(60)), cos_rad(grad2rad(45)), cos_rad(grad2rad(40))))
