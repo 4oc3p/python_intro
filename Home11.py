d606431ca6129018eb59ed28ce038f5ed2c1bd60
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 05:41:48 +0300

    Homework11

---
+++ Home11.py
@@ -0,0 +1,22 @@
+import math
+
+# Ввод сторон треугольника
+input_width = float(input("Введите ширину треугольника: "))
+input_height = float(input("Введите высоту треугольника: "))
+
+
+# Функция вычисления площади
+def square(a, b):
+    return round(0.5 * a * b, 2)
+
+
+# Функция вычисления периметра
+def perimeter(a, b):
+    c = math.sqrt(pow(a, 2) + pow(b, 2))  # вычисление третьей стороны по теореме Пифагора
+    return round(a + b + c, 2)
+
+
+# Вывод
+print("Площадь треугольника со сторонами %dсм и %dсм равняется %sсм, а периметр равен %sсм" % (
+    input_width, input_height, square(input_width, input_height), perimeter(input_width, input_height)))
+
