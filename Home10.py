4b914b38fa7e9d940115a7904a2eee0f75d9b2d0
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 05:08:31 +0300

    Homework10

---
+++ Home10.py
@@ -0,0 +1,14 @@
+# Ввод числа
+number_input = input("Введите 3-х значное число: ")
+
+
+# Функция суммы цифр 3-х значного числа
+def count(plus):
+    return int(plus[0]) + int(plus[1]) + int(plus[2])
+
+
+# Проверка на 3 знака и вывод
+if 99 < int(number_input) < 1000:
+    print("Сумма цифр числа %s равняется %d" % (number_input, count(number_input)))
+else:
+    print("Число не 3-х значное")
