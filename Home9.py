0e90f7ded20b4d331ad1ef40f1dd874193837817
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 16:21:37 +0300

    Homework9_v3

--- Home9.py
+++ Home9.py
@@ -8,8 +8,8 @@ def grad2rad(grad):
 
 # Функция cos(rad)
 def cos_rad(rad):
-    return math.cos(rad)
+    return math.cos(grad2rad(rad))
 
 
 # Вывод результата
-print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (cos_rad(grad2rad(60)), cos_rad(grad2rad(45)), cos_rad(grad2rad(40))))
+print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (cos_rad(60), cos_rad(45), cos_rad(40)))
