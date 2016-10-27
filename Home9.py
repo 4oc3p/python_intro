01e612b5fddcc389f8dff7a6bce9b3f5c4284ea2
Vladimir Loktev <l4oc3pl@gmail.com>
2016-10-27 17:49:48 +0300

    Homework9_v3

--- Home9.py
+++ Home9.py
@@ -12,4 +12,5 @@ def cos_rad(rad):
 
 
 # Вывод результата
-print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (cos_rad(60), cos_rad(45), cos_rad(40)))
+print("Вычисление cos по градусам:\ncos(60) = %.4f\ncos(45) = %.4f\ncos(40) = %.4f" % (
+    cos_rad(60), cos_rad(45), cos_rad(40)))
