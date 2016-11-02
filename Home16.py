4b74a68f840d2d9af034cfa5eef22fb5abb61dde
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-03 00:43:21 +0200

    Homework16_v2

--- Home16.py
+++ Home16.py
@@ -1,11 +1,13 @@
+# Ввод символов пользователем
 symb1 = input("Первый символ: ")
 symb2 = input("Второй символ: ")
 
 
+# Ф-я суммы числовых значений символов
 def sum_of_symbols(a, b):
-    num1, num2 = ord(a), ord(b)
+    num1, num2 = ord(a), ord(b)  # Перевод символа в число
     total = 0
-    while abs(num1 - num2) > 1:
+    while abs(num1 - num2) > 1:  # Проверка, действительно ли пользователь ввел не соседние либо одинаковые символы
         if num1 < num2:
             for i in range(num1, num2+1):
                 total += i
@@ -15,5 +17,7 @@ def sum_of_symbols(a, b):
         return total
     return 0
 
+
+# Вывод результата
 print("Сумма числовых значений символов %s + %s и всех символов между ними равняется: %d" % (
     symb1, symb2, sum_of_symbols(symb1, symb2)))
