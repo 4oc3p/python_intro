f4cf945c5aba1f0fc0def35486c29b9ffdef7257
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-03 17:29:05 +0200

    Homework10.2_v2

--- Home10.2.py
+++ Home10.2.py
@@ -1,6 +1,6 @@
 # Ввод числа
-number_input = int(input("Введите 3-х значное число: "))
-
+number_input1 = int(input("Введите 3-х значное число: "))
+number_input2 = int(input("Введите любое число: "))
 
 # Функция суммы цифр 3-х значного числа №2
 def count2(plus):
@@ -8,7 +8,20 @@ def count2(plus):
 
 
 # Проверка на 3 знака и вывод
-if 99 < number_input < 1000:
-    print("Сумма цифр числа %d равняется %d" % (number_input, count2(number_input)))
+if 99 < number_input1 < 1000:
+    print("Сумма цифр числа %d равняется %d" % (number_input1, count2(number_input1)))
 else:
     print("Число не 3-х значное")
+
+
+# Ф-я с циклом суммы цифр числа
+def sum_of_digits(a):
+    total = 0
+    while a > 0:
+        total += a % 10
+        a //= 10
+    return total
+
+
+# Вывод второй ф-ии
+print("Сумма цифр числа %d равняется %d" % (number_input2,sum_of_digits(number_input2)))
