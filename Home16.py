cbfd4d7127154238c86967a4329899e0d7a9733c
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-03 13:37:43 +0200

    Homework16_v3

--- Home16.py
+++ Home16.py
@@ -6,16 +6,11 @@ symb2 = input("Второй символ: ")
 # Ф-я суммы числовых значений символов
 def sum_of_symbols(a, b):
     num1, num2 = ord(a), ord(b)  # Перевод символа в число
-    total = 0
-    if num1 < num2:
-        pass
-    else:
-        num1, num2 = num2, num1
-    while num2 - num1 > 1:  # Проверка, действительно ли пользователь ввел не соседние либо одинаковые символы
-        for i in range(num1, num2+1):
-            total += i
-        return total
-    return 0
+    min_num = min(num1, num2)
+    max_num = max(num1, num2)
+    while max_num - min_num > 1:  # Проверка, действительно ли пользователь ввел не соседние либо одинаковые символы
+        return sum(range(min_num, max_num + 1))
+    return min_num
 
 
 # Вывод результата
