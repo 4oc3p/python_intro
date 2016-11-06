b684c773991b74995cf68c92e35f9871b7ce77f9
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-07 00:19:32 +0200

    Homework21_v3

--- Home21.py
+++ Home21.py
@@ -5,7 +5,7 @@ end = int(input("Конечное число: "))
 # Ф-я для проверки наличия цифр 1 или 7 в числе
 def is_1and7_in(a, b):
     for i in range(a, b+1):
-        has1, has7 = bool, bool
+        has1, has7 = False, False
         work = i
         while work > 0:
             check_last = work % 10
@@ -14,7 +14,7 @@ def is_1and7_in(a, b):
                 has1 = True
             if check_last == 7:
                 has7 = True
-        if has1 is True and has7 is True:
+        if has1 and has7:
             print(i)
 
 
