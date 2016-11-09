09ac531db69c750aba767ca4b2162334b9f9d55e
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-10 00:31:49 +0200

    Homework23

--- Home23.py
+++ Home23.py
@@ -1,7 +1,7 @@
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lst = [1, 2, 3, 42, 51, 65, 74, 83, 92, 102]
 
 
 def average(l):
     return sum(l) / len(l)
 
-print(average(lst))
+print("Среднее арифметическое чисел %s = %s" % (lst, average(lst)))
