8808a4f2bd2c54e020be57614c0ed23657194ef4
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-12 02:04:52 +0200

    Homework28_v2

--- Home28.py
+++ Home28.py
@@ -5,12 +5,15 @@ l2 = ['а', 'б', 'в', 'г', 'д', 'о', '1', '2', '3', '4', '5', '6', '-', '0'
 def encrypt(lst1, lst2):
     s = ""
     for i in range(0, len(lst1)):
-        num = 0
-        while True:
-            if lst1[i] == lst2[num]:
-                s += lst2[num + 5]
-                break
-            num += 1
+        if lst1[i] in lst2:
+            a = lst2.index(lst1[i])
+            s += lst2[a+5]
+        # num = 0
+        # while True:
+        #     if lst1[i] == lst2[num]:
+        #         s += lst2[num + 5]
+        #         break
+        #     num += 1
     return s
 
 
