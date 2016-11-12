b55625ca62be03bca0e60a1cc5a7d83fe539264f
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-12 02:17:13 +0200

    Homework29_v2

--- Home29.py
+++ Home29.py
@@ -1,22 +1,26 @@
 import random
 
+user_input = int(input("Введите количество знаков для генерации пароля: "))
 
-def password():
-    lower = list("abcdefghijklmnopqrstuvwxyz")
-    upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+def password(a):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    lower = list(alphabet)
+    upper = list(alphabet.upper())
     digits = list("1234567890_")
     s = []
     # s = lower + upper + digits
     # random.shuffle(s)
     # return ''.join(s[0:a])
-    while len(s) < 8:
-        if len(s) < 3:
-            s += lower[random.randint(0, len(lower)-1)]
-        elif len(s) < 6:
-            s += upper[random.randint(0, len(upper)-1)]
+    while len(s) < a:
+        if len(s) < a * 0.33:
+            s += random.choice(lower)
+        elif len(s) < a * 0.66:
+            s += random.choice(upper)
         else:
-            s += digits[random.randint(0, len(digits)-1)]
+            s += random.choice(digits)
     random.shuffle(s)
     return ''.join(s)
 
-print("Сгенерированный пароль:", password())
+
+print("Сгенерированный пароль:", password(user_input))
