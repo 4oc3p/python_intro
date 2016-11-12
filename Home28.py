dfbd75e1ae2ba0c8f855541d9bd960a07c478991
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-12 06:14:30 +0200

    Homework28_v3

--- Home28.py
+++ Home28.py
@@ -3,18 +3,17 @@ l2 = ['а', 'б', 'в', 'г', 'д', 'о', '1', '2', '3', '4', '5', '6', '-', '0'
 
 
 def encrypt(lst1, lst2):
-    s = ""
+    s = []
     for i in range(0, len(lst1)):
         if lst1[i] in lst2:
-            a = lst2.index(lst1[i])
-            s += lst2[a+5]
+            s.append(lst2[(lst2.index(lst1[i]) + 5)])
         # num = 0
         # while True:
         #     if lst1[i] == lst2[num]:
         #         s += lst2[num + 5]
         #         break
         #     num += 1
-    return s
+    return ''.join(s)
 
 
 print("Зашифрованная фраза:", encrypt(l1, l2))
