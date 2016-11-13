44cbf54856e9e1daad30809acf97926296e5fd83
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-13 12:42:18 +0200

    Homework28_v4

--- Home28.py
+++ Home28.py
@@ -1,12 +1,12 @@
 l1 = list(input("Введите строку для шифровки: "))
-l2 = ['а', 'б', 'в', 'г', 'д', 'о', '1', '2', '3', '4', '5', '6', '-', '0']*2
+l2 = ['а', 'б', 'в', 'г', 'д', 'о', '1', '2', '3', '4', '5', '6', '-', '0']
 
 
 def encrypt(lst1, lst2):
     s = []
     for i in range(0, len(lst1)):
         if lst1[i] in lst2:
-            s.append(lst2[(lst2.index(lst1[i]) + 5)])
+            s.append(lst2[(lst2.index(lst1[i]) + 5) % len(lst2)])
         # num = 0
         # while True:
         #     if lst1[i] == lst2[num]:
