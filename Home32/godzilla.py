3ed639e66d680ed7bb14a4173aca0c842c6cdc94
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-29 19:21:48 +0200

    Homework32_0.5

--- Home32/godzilla.py
+++ Home32/godzilla.py
@@ -3,23 +3,28 @@ from Home32 import human
 
 class Godzilla:
 
-    def __init__(self, stomach_size, name="Godzilla"):
+    def __init__(self, stomach_limit, name="Godzilla"):
         self.name = name
-        self.stomach_size = stomach_size
-        self.empty_stomach = 0
-        self.hum_weight = 0
+        self.stomach_limit = stomach_limit
+        self.stomach_size = 0
+        self.last_hum_weight = 0
+
+    def is_stomach_full(self):
+        if self.stomach_size >= self.stomach_limit * 0.9:
+            return True
+        else:
+            return False
 
     def eat_people(self, human):
-        self.hum_weight = human.weight
-        if self.empty_stomach > self.stomach_size * 0.9:
-            pass
+        self.last_hum_weight = human.weight
+        if Godzilla.is_stomach_full(self):
+            print("Не могу больше есть!!! Желудок забит на 90%")
         else:
-            self.empty_stomach += self.hum_weight
+            self.stomach_size += self.last_hum_weight
 
-    def print(self):
-        if self.empty_stomach < self.stomach_size * 0.9:
-            print("Чудовище, по имени %s съело ещё одного человеа весом %sкг и заполнила желудок на %s%%" %
-                  (self.name, self.hum_weight, round(self.empty_stomach / self.stomach_size * 100)))
+    def print_status(self):
+        if Godzilla.is_stomach_full(self):
+            print("%s уже наелась и хочет спать." % self.name)
         else:
-            print("Чудовище уже не может больше есть! Желудок заполнен на %s%%" %
-                  round(self.empty_stomach / self.stomach_size * 100))
+            print("Чудовище, по имени %s съело человека весом %sкг и желудок заполнен на %s%%" %
+                  (self.name, self.last_hum_weight, round(self.stomach_size / self.stomach_limit * 100)))
--- Home32/human.py
+++ Home32/human.py
@@ -4,4 +4,4 @@ from random import randint
 class Human:
 
     def __init__(self):
-        self.weight = randint(45, 110)
+        self.weight = randint(45, 100)
