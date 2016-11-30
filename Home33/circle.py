64b3fc814cced7328746c578652d19e31af2ea0a
Vladimir Loktev <l4oc3pl@gmail.com>
2016-11-30 15:50:15 +0200

    Homework33_v1

--- Home33/circle.py
+++ Home33/circle.py
@@ -8,20 +8,12 @@ class Circle:
         self.center_x = x
         self.center_y = y
         self.radius = radius
-        self.point_x = 0
-        self.point_y = 0
 
-    def is_point_in_circle(self):
-        return sqrt((self.point_x - self.center_x)**2 + (self.point_y - self.center_y)**2) <= self.radius
-
-    def result_print(self, point):
-        self.point_x = point.x
-        self.point_y = point.y
-        if self.is_point_in_circle():
-            result = "принадлежит"
-        else:
-            result = "не принадлежит"
+    def is_point_of_circle(self, point):
+        checking = sqrt((point.x - self.center_x)**2 + (point.y - self.center_y)**2) <= self.radius
+        result = ("не принадлежит", "принадлежит")[checking]
         print("Точка с координатами x=%s, y=%s %s окружности с центром x=%s, y=%s и радиусом %s." %
-                  (self.point_x, self.point_y, result, self.center_x, self.center_y, self.radius))
+              (point.x, point.y, result, self.center_x, self.center_y, self.radius))
+        return checking
 
 
--- Home33/main.py
+++ Home33/main.py
@@ -5,5 +5,5 @@ point1 = point.Point(6, 0)
 point2 = point.Point(3, 6)
 circle1 = circle.Circle(1, 4, 5)
 
-circle1.result_print(point1)
-circle1.result_print(point2)
+circle1.is_point_of_circle(point1)
+circle1.is_point_of_circle(point2)
