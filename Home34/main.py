6ce5c26f55b028c0baf52af5571dc91159560101
Vladimir Loktev <l4oc3pl@gmail.com>
2016-12-01 23:57:18 +0200

    Homework34_v1.1

--- Home34/main.py
+++ Home34/main.py
@@ -1,13 +1,33 @@
 from Home34 import vehicle, car, plane
 
-car1 = car.Car(180, 11, 76, 5, "Japan")
-car1.print_info()
+car1 = car.Car(speed_limit=180,
+               fuel_consumption=11,
+               fuel_tank_size=76,
+               number_of_seats=5,
+               country_of_origin="Japan")
 
-car2 = car.Car(160, 15, 89, 9, "USA")
-car2.print_info()
+car2 = car.Car(speed_limit=160,
+               fuel_consumption=15,
+               fuel_tank_size=89,
+               number_of_seats=9,
+               country_of_origin="USA")
 
-plane1 = plane.Plane(600, 400, 20000, 120, 13450, 15000)
-plane1.print_info()
+plane1 = plane.Plane(speed_limit=600,
+                     fuel_consumption=400,
+                     fuel_tank_size=20000,
+                     number_of_seats=120,
+                     service_ceiling=13450,
+                     current_alt=12222)
 
-plane2 = plane.Plane(740, 420, 24330, 154, 16200, 16400)
+plane2 = plane.Plane(speed_limit=740,
+                     fuel_consumption=420,
+                     fuel_tank_size=24330,
+                     number_of_seats=154,
+                     service_ceiling=16200,
+                     current_alt=15000)
+
+
+# car1.print_info()
+# car2.print_info()
+# plane1.print_info()
 plane2.print_info()
--- Home34/plane.py
+++ Home34/plane.py
@@ -8,21 +8,21 @@ class Plane(Vehicle):
     def __init__(self, speed_limit, fuel_consumption, fuel_tank_size, number_of_seats, service_ceiling, current_alt):
         Vehicle.__init__(self, speed_limit, fuel_consumption, fuel_tank_size, number_of_seats)
         self.service_ceiling = service_ceiling
-        # self._current_alt = 0
+        self._current_alt = current_alt
         self.current_alt = current_alt
         Plane.id += 1
         self.id = Plane.id
 
-    # @property
-    # def current_alt(self):
-    #     return self._current_alt
-    #
-    # @current_alt.setter
-    # def current_alt(self, current_alt):
-    #     if self.current_alt < self.service_ceiling:
-    #         self._current_alt = current_alt
-    #     else:
-    #         self.decrease_alt()
+    @property
+    def current_alt(self):
+        return self._current_alt
+
+    @current_alt.setter
+    def current_alt(self, current_alt):
+        if self.current_alt > self.service_ceiling:
+            self._current_alt = "error"
+        else:
+            self._current_alt = current_alt
 
     def decrease_alt(self):
         if self.current_alt <= self.service_ceiling:
@@ -36,5 +36,5 @@ class Plane(Vehicle):
     def print_info(self):
         print("-Самолёт №%s\n"
               "-Максимальная высота, м: %d\n"
-              "-Текущая высота, м: %d" % (self.id, self.service_ceiling, self.current_alt))
+              "-Текущая высота, м: %s" % (self.id, self.service_ceiling, self.current_alt))
         Vehicle.print_info(self)
